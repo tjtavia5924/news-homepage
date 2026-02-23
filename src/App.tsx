bec0ad5e100f6cff4f851b85ed6cd939a15573ae
tjtavia5924 <taviathompson@hotmail.co.uk>
2026-02-23 21:05:57 +0000

    Upgraded eslint, to overcome npm i error, defined container to hold upcoming items and imported main file into html.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+  <div className="flex justify-center items-center h-screen">
+ <div className=" box-border w-[1112px] h-[811px]">App</div>
+
+  </div>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   base: "/contact-form/",
+ //  base: "/news-homepage/",
 })
