cab2b38b0d7c107b7a6bc94389b80b94ff939073
tjtavia5924 <taviathompson@hotmail.co.uk>
2026-03-11 18:46:05 +0000

    positioned and styled text in main.tsx. WIP

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,14 @@ export default function Main() {
     <>
     <div className="mt-[64px]">
     <img src={contents.image} alt="main image" className='w-[730px] h-[300px]'/>
-    <div>{contents.description}</div>
+      <div className="w-[730px] h-[182px] mt-[24px] flex">  
+      <div className="font-extrabold text-custom-very-dark-blue text-[54px] w-[350px]">    
+      {contents.title}
+      </div>
+      <div className="w-[380px] ml-[32px]">
+      {contents.description}
+      </div>
+      </div>
     </div>
     </>
   )
